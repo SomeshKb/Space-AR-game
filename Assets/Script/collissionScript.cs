34a57685def9fd60450f58ac92be619177893c6c
Someshkb <someshkumarbhaskar@gmail.com>
2018-03-23 15:00:31 +0530

    AR
    first commit

---
+++ Assets/Script/collissionScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class collissionScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+
+		GameObject explosion = Instantiate (Resources.Load ("FlareMobile", typeof (GameObject))) as GameObject;
+		explosion.transform.position = transform.position;
+		Destroy(col.gameObject);
+		Destroy (gameObject);
+
+	}
+}
